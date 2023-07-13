bedb801453be1c566c772f5cb2d5ce2b21c1741f
GotsmanRoman <96074289+GotsmanRoman@users.noreply.github.com>
2023-07-13 19:15:08 +0300

    Update isValidId.js
    removing console.log

--- validate/isValidId.js
+++ validate/isValidId.js
@@ -3,7 +3,6 @@ const { HttpError } = require("../helpers");
 
 const isValidId = (req, res, next) => {
   const { contactId } = req.params;
-  console.log(contactId);
   console.log(isValidObjectId(contactId));
   if (!isValidObjectId(contactId)) {
     next(HttpError(400, `${contactId} is not correct type of ID`));
